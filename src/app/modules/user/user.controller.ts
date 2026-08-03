6824ef090be804b529c8d67fabfc6bbc3ba22054
MithilaKhan <mithilakhan082@gmail.com>
2026-08-03 21:43:26 +0600

    feat: complete configuration and authentication setup

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,65 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../../shared/catchAsync';
+import { getSingleFilePath } from '../../../shared/getFilePath';
+import sendResponse from '../../../shared/sendResponse';
+import { UserService } from './user.service';
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { ...userData } = req.body;
+    const result = await UserService.createUserToDB(userData,res);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'User created successfully',
+      data: result,
+    });
+  }
+);
+
+const getUserProfile = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user;
+  const result = await UserService.getUserProfileFromDB(user);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'Profile data retrieved successfully',
+    data: result,
+  });
+});
+
+//update profile
+const updateProfile = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+    let image = getSingleFilePath(req.files, 'image');
+    
+    const data = {
+      image,
+      ...req.body,
+    };
+    const result = await UserService.updateProfileToDB(user, data);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Profile updated successfully',
+      data: result,
+    });
+  }
+);
+
+const uploadFile = catchAsync(async (req: Request, res: Response) => {
+  const file = getSingleFilePath(req.files, 'image');
+  return sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'File uploaded successfully',
+    data: file,
+  })
+});
+
+export const UserController = { createUser, getUserProfile, updateProfile, uploadFile };
